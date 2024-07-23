b8eef3edb8e2f3608080c81ef2ca264f18a5a002
Regi Witanto <witantoregi@gmail.com>
2024-07-23 22:51:20 +0700

    create verify note access func in notes service

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -6,8 +6,9 @@ const { mapDBToModel } = require('../../utils');
 const AuthorizationError = require('../../exceptions/AuthorizationError');
 
 class NotesService {
-  constructor() {
+  constructor(collaborationService) {
     this._pool = new Pool();
+    this._collaborationsService = new CollaborationsService();
   }
 
   async addNote({ title, body, tags, owner }) {
@@ -92,6 +93,21 @@ class NotesService {
     const note = result.rows[0];
     if (note.owner !== owner) {
       throw new AuthorizationError('Anda tidak berhak mengakses resource ini');
+    }
+  }
+
+  async verifyNoteAccess(noteId, userId) {
+    try {
+      await this.verifyNoteOwner(noteId, userId);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        throw error;
+      }
+      try {
+        await this._collaborationService.verifyCollaborator(noteId, userId);
+      } catch {
+        throw error;
+      }
     }
   }
 }
