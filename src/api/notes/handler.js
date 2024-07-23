640c268f4a670df7138d91d48078f34de9d98deb
Regi Witanto <witantoregi@gmail.com>
2024-07-23 10:36:27 +0700

    add credential id in put note by id handler

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -64,7 +64,9 @@ class NotesHandler {
   async putNoteByIdHandler(request, h) {
     this._validator.validateNotePayload(request.payload);
     const { id } = request.params;
+    const { id: credentialId} = request.auth.credentials;
 
+    await this._service.verifyNoteOwner(id, credentialId);
     await this._service.editNoteById(id, request.payload);
 
     return {
